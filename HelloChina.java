5bad2add888eb292b4fea3e626d93475e280a8af
NeverNG <wanghong_hui@live.com>
2025-01-10 11:30:57 +0800

    vue初始化

---
+++ HelloChina.java
@@ -0,0 +1,44 @@
+class HelloChina{
+	public static void main(String[] args){
+		/*数据类型 */
+		/*变量 */
+		/*整型 */
+		byte a = 10;
+		System.out.println(a);
+		short b_1 = 20;
+		System.out.println(b_1);
+		int $a = 40;
+		System.out.println($a);
+		long _d = 509999999999L;
+		System.out.println(_d);
+		float f = 3.14F;
+		System.out.println(f);
+		double dd = 3.1415926;
+		System.out.println(dd);
+		char c = 'a';
+		System.out.println(c);
+		boolean g = true;
+		System.out.println(g);
+		boolean h = false;
+		System.out.println(h);
+
+		int i;
+		i = 10;
+		System.out.println(i);
+		{
+			int j = 20;
+			System.out.println(j);
+		}
+		//System.out.println(j);
+		int aa = 10;
+		int bb = 20;
+		int cc = aa + bb;
+		System.out.println(cc);
+
+		byte ff = 30;
+		int ee = 40;
+		int gg = ff + ee;
+		System.out.println(gg);
+
+	}
+}
